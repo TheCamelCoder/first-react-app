36a3d5a44b5201245e03fbb1b6ec55c98957fb68
Ahmed SanaUllah <ahmedsanaullah@Ahmeds-MBP.home>
2020-12-12 02:24:03 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import TestComponent from "./TestComponent";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <h1>Hello Ahmed</h1>
+      </div>
+      <div>
+        <TestComponent />
+      </div>
+    </div>
+  );
+}
+
+export default App;
